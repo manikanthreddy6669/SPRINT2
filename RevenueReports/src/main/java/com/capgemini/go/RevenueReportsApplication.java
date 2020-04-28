4cff50b0aaa2bc21f7649d31ed63e22dfed83a7f
manikanthreddy6669 <manikanthreddy669@gmail.com>
2020-04-28 19:53:25 +0530

    With SpringBootAdmin,CloudConfig,Zipkins and Sleuth

---
+++ RevenueReports/src/main/java/com/capgemini/go/RevenueReportsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenueReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevenueReportsApplication.class, args);
+	}
+
+}

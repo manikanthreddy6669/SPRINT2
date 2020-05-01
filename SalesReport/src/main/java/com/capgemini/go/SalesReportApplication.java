f34fedffe331d18fe3d8a2a4fae56417f4803572
manikanthreddy6669 <manikanthreddy669@gmail.com>
2020-05-01 12:27:57 +0530

    Added Eureka

--- SalesReport/src/main/java/com/capgemini/go/SalesReportApplication.java
+++ SalesReport/src/main/java/com/capgemini/go/SalesReportApplication.java
@@ -2,8 +2,10 @@ package com.capgemini.go;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SalesReportApplication {
 
 	public static void main(String[] args) {
